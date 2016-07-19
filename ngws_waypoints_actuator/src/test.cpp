03ef78cca69abdf27c1205ff136813e7b573a27f
yangxiao1652 <1652923256@qq.com>
2016-07-19 10:26:51 +0800

    test

--- ngws_waypoints_actuator/src/test.cpp
+++ ngws_waypoints_actuator/src/test.cpp
@@ -8,23 +8,42 @@
 //int biaozhi=1;
 int main(int argc, char **argv)
 { 
-  ros::init(argc, argv, "test");
+  int biaozhi=1;
+  ros::init(argc, argv, "test1");
   ros::NodeHandle n;
   ros::Publisher chatter_pub = n.advertise<nav_msgs::Odometry>("odom_fused", 1000);
-
-  nav_msgs::Odometry msg
+ 
+//  ros::Publisher position_pub = n.advertise<ngws_msgs::WayPointArray>("cmd_waypoints", 1000);
+nav_msgs::Odometry msg;
+//ngws_msgs::WayPointArray waypoint_msg[3];
+while((ros::ok()) ){
+if(biaozhi==1){
+ 
+ // nav_msgs::Odometry msg;
+ 
   msg.pose.pose.position.x=0.0;
   msg.pose.pose.position.y=0.0;
   msg.pose.pose.orientation.z=0.0;
-  ros::Rate loop_rate(10);
-  while (ros::ok())
-  {
-    if(biaozhi==1)
-    { 
-   	chatter_pub.publish(msg);
-   	biaozhi=0;
-     }   
+
+//  ngws_msgs::WayPointArray waypoint_msg;
+/* waypoint_msg.waypoints[0].waypoint.position.x=0;
+ waypoint_msg.waypoints[0].waypoint.position.y=0;
+ 
+ waypoint_msg.waypoints[1].waypoint.position.x=0;
+ waypoint_msg.waypoints[1].waypoint.position.y=1;
+  
+ waypoint_msg.waypoints[2].waypoint.position.x=1;
+ waypoint_msg.waypoints[2].waypoint.position.x=1;
+  
+
+ 
+       position_pub.publish(msg);*/
+       chatter_pub.publish(msg);
+       
+  	biaozhi=0;
+    }
+      }
     ros::spinOnce();
-    loop_rate.sleep();  
-  }
+
+
 }
